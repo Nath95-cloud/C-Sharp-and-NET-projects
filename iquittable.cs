2c87140b9bc230a8548d0ad2e82904318a9a5d9d
Nath95-cloud <nathantaylor461@gmail.com>
2025-02-10 16:10:21 +0000

    Add files via upload

---
+++ iquittable.cs
@@ -0,0 +1,33 @@
+using System;
+namespace IQuittable{
+    class program{
+    public class Employee quit assignment{
+    public string firstname {get;set; }
+    public string lastname {get;set;}
+    public abstract void Quit();
+    }
+}
+}
+using system;
+namespace IQuittable
+    public class Employee : Person{
+        public int id {get;set;}
+        public override void Quit();
+    {
+    Console.WriteLine('Name' + firstname + '' + lastname)
+    Console.WriteLine('Would You like to quit your job?');
+    Console.WriteLine('YES or No?')
+    Console.ReadLine();
+    Console.Writeline('You Have selected YES');
+    }
+}
+using system;
+namespace iquittable{
+    class programm{
+        static void Main(string[]args){
+            Employee sam = old Employee(){firstname ="Sample", lastName = "Student"};
+            sam.Quit();
+            Console.Readline
+        } 
+    }
+}
